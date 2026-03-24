f9525fa6dc97675749c066a65c8d6e41829b3798
Sithija-R <sithijarathukorale@gmail.com>
2026-03-24 15:23:37 +0530

    profile screen UI

--- Practical2/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ Practical2/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -5,16 +5,27 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
@@ -25,48 +36,47 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyApplicationTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    ProfileScreen(modifier = Modifier.padding(innerPadding))
                 }
-
             }
-
         }
     }
 }
 
-
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyApplicationTheme {
-        Greeting("Android")
-
+fun ProfileScreen(modifier: Modifier = Modifier){
+    Column (
+        modifier = modifier.fillMaxSize().padding(32.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ){
+        ProfileTitle()
+        ProfileImage()
+        ProfileDescription()
     }
 }
 
 @Composable
-fun Hello() {
+fun ProfileTitle(){
     Text(
-        text = "Hello Students",
-        modifier = Modifier.background(Color.Cyan),
-        color = Color.Black,
-        fontSize = 10.sp, fontStyle = FontStyle.Italic
-   )
+        text = "John Doe",
+        modifier = Modifier.padding(16.dp)
+    )
 }
 
 @Composable
-@Preview(showBackground = true)
-fun HelloPreview() {
-    Hello()
+fun ProfileImage(){
+    Icon(
+        imageVector = Icons.Default.AccountCircle,
+        contentDescription = "Profile Image",
+        modifier = Modifier.padding(16.dp).size(120.dp),
+        tint = MaterialTheme.colorScheme.onPrimaryContainer
+    )
+}
+
+@Composable
+fun ProfileDescription(){
+    Text(
+        text = "Professional Mobile Developer",
+        modifier = Modifier.padding(16.dp)
+    )
 }
